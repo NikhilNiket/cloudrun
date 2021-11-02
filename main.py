3a95ffeae524e9b2faabce1a338427bead3360fc
NikhilNiket <64312817+NikhilNiket@users.noreply.github.com>
2021-11-02 16:06:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    name = request.args.get('name', 'change 1')
+    name = request.args.get('name', 'change 2')
     return f'Hello {name}!'
 
 
